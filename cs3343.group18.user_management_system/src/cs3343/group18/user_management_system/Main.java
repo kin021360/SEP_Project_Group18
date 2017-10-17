5bdd4afd8eae6123efccf69b1ac7f9940225cde0
Nathan Lam_desktop <kin021360@yahoo.com.hk>
2017-10-17 23:45:58 +0800

    initial java project commit

---
+++ cs3343.group18.user_management_system/src/cs3343/group18/user_management_system/Main.java
@@ -0,0 +1,10 @@
+package cs3343.group18.user_management_system;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ cs3343.group18.user_management_system/src/cs3343/group18/user_management_system/data/JsonDao.java
@@ -0,0 +1,5 @@
+package cs3343.group18.user_management_system.data;
+
+public abstract class JsonDao {
+
+}
