a6d69e0f792480ded8de76cb4946a32fe72bbaae
Nathan Lam_desktop <kin021360@yahoo.com.hk>
2017-10-18 23:05:55 +0800

    update project

--- cs3343.group18.user_management_system/src/cs3343/group18/user_management_system/Main.java
+++ cs3343.group18.user_management_system/src/cs3343/group18/user_management_system/Main.java
@@ -1,10 +1,12 @@
 package cs3343.group18.user_management_system;
 
+import cs3343.group18.user_management_system.data.UserDao;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO interface
+		System.out.println("test main");
+		
 	}
-
 }
---
+++ cs3343.group18.user_management_system/src/cs3343/group18/user_management_system/data/UserDao.java
@@ -0,0 +1,15 @@
+package cs3343.group18.user_management_system.data;
+
+/**
+ * Created by as on 18/10/2017.
+ */
+public class UserDao extends JsonDao {
+
+    public void test() {
+        try {
+            readJsonFile("thread_338423.json");
+        } catch (Exception e) {
+
+        }
+    }
+}
