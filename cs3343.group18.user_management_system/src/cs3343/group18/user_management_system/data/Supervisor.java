4b8a3574e51a306ad8dfe72d6603ca760ea2bfab
NathanLam <nathanlam@seekasia.com>
2017-10-24 17:47:07 +0800

    update user supervistor class

--- cs3343.group18.user_management_system/src/cs3343/group18/user_management_system/data/Supervisor.java
+++ cs3343.group18.user_management_system/src/cs3343/group18/user_management_system/data/Supervisor.java
@@ -2,9 +2,13 @@ package cs3343.group18.user_management_system.data;
 
 import java.util.ArrayList;
 
-public class Supervisor extends User {
+public class Supervisor extends User implements ISupervisorInfo{
     public Supervisor(String n, String pwd, String gen, EnumPosition po, int sID, String em) {
-        super(n, pwd, gen, po, sID, em);
+        super(n, pwd, gen, po, sID, em, null);
     }
 
+    @Override
+    public boolean isMySubordinate(IUserInfo subordinate) {
+        return false;
+    }
 }
--- cs3343.group18.user_management_system/src/cs3343/group18/user_management_system/data/User.java
+++ cs3343.group18.user_management_system/src/cs3343/group18/user_management_system/data/User.java
@@ -1,8 +1,5 @@
 package cs3343.group18.user_management_system.data;
 
-import com.sun.org.apache.bcel.internal.generic.IUSHR;
-
-import javax.lang.model.element.NestingKind;
 
 public class User implements IUserInfo {
 
@@ -12,14 +9,16 @@ public class User implements IUserInfo {
     EnumPosition position;
     private int staffId;
     private String email;
+    private ISupervisorInfo supervisor;
 
-    public User(String n, String pwd, String gen, EnumPosition po, int sID, String em) {
+    public User(String n, String pwd, String gen, EnumPosition po, int sID, String em, ISupervisorInfo supervisor) {
         name = n;
         password = pwd;
         gender = gen;
         position = po;
         staffId = sID;
         email = em;
+        this.supervisor = supervisor;
     }
 
 
@@ -29,10 +28,7 @@ public class User implements IUserInfo {
 
 
     public boolean checkPassword(String input) {
-        if (input == password)
-            return true;
-        else
-            return false;
+        return input.equals(password);
     }
 
     public int getStaffId() {
@@ -49,12 +45,13 @@ public class User implements IUserInfo {
     }
 
     //Need to revise
-  public  String showAllPermissions(){
-     return null;
-  }
+    public String showAllPermissions() {
+        return null;
+    }
+
     //Need to revise
-    public ISupervisorInfo getSupervisorInfo(){
-      return  null;
+    public ISupervisorInfo getSupervisorInfo() {
+        return supervisor;
     }
 
 
