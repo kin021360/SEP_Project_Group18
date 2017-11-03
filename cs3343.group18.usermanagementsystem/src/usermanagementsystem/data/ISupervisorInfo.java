5f62efc57f1094a5e9e424531a4024c14420178d
Nathan Lam_desktop <kin021360@yahoo.com.hk>
2017-11-03 22:24:58 +0800

    move class into diff package

--- cs3343.group18.usermanagementsystem/src/usermanagementsystem/data/ISupervisorInfo.java
+++
@@ -1,8 +0,0 @@
-package usermanagementsystem.data;
-
-/**
- * Created by nathanlam on 24/10/2017.
- */
-public interface ISupervisorInfo extends IUserInfo {
-    boolean isMySubordinate(IUserInfo subordinate);
-}
