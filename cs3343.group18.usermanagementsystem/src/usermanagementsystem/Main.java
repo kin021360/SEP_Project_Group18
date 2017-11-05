6f9f72ec6dd23e1dfd80804a00c2cc721a73e947
James Cheng <james104104@hotmail.com>
2017-11-06 00:37:13 +0800

    feature/user-login-and-menu initial basic login logout and program loop

--- cs3343.group18.usermanagementsystem/src/usermanagementsystem/Main.java
+++ cs3343.group18.usermanagementsystem/src/usermanagementsystem/Main.java
@@ -3,18 +3,145 @@ package usermanagementsystem;
 import usermanagementsystem.Menu.Menus;
 import usermanagementsystem.dataaccess.*;
 import usermanagementsystem.datastructure.*;
+import usermanagementsystem.user_login.UserLogin;
 
 import java.util.Hashtable;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        // TODO console input interface. No need un-do, re-do command <- very troublesome
-        System.out.println("test main");
-
-        Menus menu = new Menus();
-
-        menu.runMenu();
+        
+    	//login status and get user information
+    	UserLogin loginObject = UserLogin.getInstance();
+    	UserDao userDao = loginObject.getUserDao();
+    	
+    	//load User and Supervisor list(Hashtable) without mapping their Supervisor or Subordinate yet
+    	Hashtable<String, User> users = userDao.loadUsersWithoutSupervisor();
+    	Hashtable<String, Supervisor> supervisors = userDao.loadSupervisorsWithoutUser();
+    	
+    	//interface (menu and options)
+    	Menus menu = new Menus();
+    	
+    	String username = "", password = "", choice = "";
+    	int option;
+    	boolean isAdmin, invalidOption = false;
+  	   	Scanner scannerObj = new Scanner(System.in);
+  	   	
+  	   	//keep program return to login or menu and other function
+  	   	while(loginObject.getLoginStatus() == true || loginObject.getLoginStatus() == false) {
+  	   		
+  	   		//before login
+  	   		if(!loginObject.getLoginStatus()) {
+		  	   	System.out.print("Username: ");
+		  	   	username = scannerObj.next();
+		  	   	System.out.print("Password: ");
+			   	password = scannerObj.next();			   	
+			   	
+			   	try {
+			   		if(users.get(username).checkPassword(password)) {
+			   			loginObject.setLoginStatus(true);
+			   			loginObject.setUsername(username);
+			   			menu.printHeader();
+			   		}
+			   		else {
+			   			System.out.println("Invalid username or password!\n");
+			   		}
+			   	}catch(NullPointerException e) {
+			   		System.out.println("Invalid username or password!\n");
+			   	}
+  	   		}
+  	   		
+  	   		//after login
+  	   		if(loginObject.getLoginStatus()) {
+  	   			isAdmin = users.get(username).isAdmin();
+  	   			
+  	   			menu.printHomeMenu();
+  	   			if(isAdmin) {
+	   				menu.printAdminHomeMenu();
+	   			}
+  	   			
+  	   			try {
+	  	   			System.out.print("Option: ");
+		  	   		option = scannerObj.nextInt();
+		  	   		
+		  	   		//normal user options
+			  	   	switch(option) {
+			  	   	case 1:
+			  	   		
+			  	   		break;
+				  	case 2:
+				  		
+				  	   	break;
+				  	case 3:
+				  		
+			  	   		break;
+				  	case 4:
+				  		
+			  	   		break;
+				  	case 5:
+						
+				 		break;
+			  	   	case 0:
+				   		//logout
+				   		while(!choice.equals("y") && !choice.equals("n")) {
+				   			System.out.print("Logout(Y/N)? ");
+				   			choice = scannerObj.next();
+					  	   	
+					  	   	String logoutMsg =  loginObject.logoutProcess(choice.toLowerCase());
+					  	   	if(logoutMsg != "") {
+					  	   		System.out.println(logoutMsg);
+					  	   	}
+				   		}
+				   		choice = "";
+		  	   			break;
+		  	   		default:
+		  	   			invalidOption = true;
+		  	   			break;
+		  	   		}
+			  	   	
+			  	   	//admin options
+			  	   	if(isAdmin && invalidOption) {
+				  	   	switch(option) {
+				  	   	case 11:
+				  	   		
+				  	   		break;
+					  	case 12:
+					  		
+					  	   	break;
+					  	case 13:
+					  		
+				  	   		break;
+					  	case 14:
+					  		
+				  	   		break;
+					  	case 15:
+							
+					 		break;
+			  	   		default:
+			  	   			invalidOption = true;
+			  	   			break;
+			  	   		}
+			  	   	}
+			  	   	
+			  	   	if(invalidOption) {
+			  	   		invalidOption = false;
+			  	   		System.out.println("Invalid option!");
+			  	   	}
+			  	   	
+  	   			}catch(InputMismatchException e) {
+  	   				//clear scanner for next option input
+  	   				scannerObj.nextLine();
+  	   				System.out.println("Invalid option!");
+	   			}
+		   	}  	   		
+  	   	}
+  	   	
+    	
+    	
+//        // TODO console input interface. No need un-do, re-do command <- very troublesome
+//        System.out.println("test main");
 //
 //
 //        //sample example to load users based on data.json
@@ -35,5 +162,6 @@ public class Main {
 //        users.get("abc").changePassword("999");
 //        supervisors.get("efg").changePassword("777");
 //        userDao.updateAndSave(users, supervisors);
+
     }
 }
--- cs3343.group18.usermanagementsystem/src/usermanagementsystem/Menu/Menus.java
+++ cs3343.group18.usermanagementsystem/src/usermanagementsystem/Menu/Menus.java
@@ -9,23 +9,31 @@ public class Menus {
         System.out.println("|    Welcome !                      |");
         System.out.println("|                                   |");
         System.out.println("+-----------------------------------+");
-        System.out.println("\n\n Please login: ");
-
     }
 
-    public void runMenu(){
-
-        while(!done){
-            printMenu();
-        }
+    //each menu for each page or function
+    
+    public void printHomeMenu() {
+    	System.out.println();
+    	System.out.println("User Options");
+        System.out.println("1) ");
+        System.out.println("2) ");
+        System.out.println("3) ");
+        System.out.println("4) ");//Read My Info
+        System.out.println("5) ");//Update My Info
+        System.out.println("0) Logout");
+        System.out.println();
     }
-
-    public void printMenu() {
-        System.out.println("1)   ");
-        System.out.println("2)   ");
-        System.out.println("3)   ");
-        System.out.println("4)   ");
-        System.out.println("0)   ");
+    
+    public void printAdminHomeMenu() {
+    	System.out.println();
+    	System.out.println("Admin Options");
+        System.out.println("11) ");//Create User
+        System.out.println("12) ");//Read User Info
+        System.out.println("13) ");//Update User Info
+        System.out.println("14) ");//Delete User
+        System.out.println("15) ");
+        System.out.println();
     }
 
 }
