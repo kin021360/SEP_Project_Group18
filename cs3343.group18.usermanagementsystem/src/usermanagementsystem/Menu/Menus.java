8ce32eba8908a330a959e6dc4bc6344c4deb6381
Nathan Lam_desktop <kin021360@yahoo.com.hk>
2017-11-06 22:09:07 +0800

    code reformat

--- cs3343.group18.usermanagementsystem/src/usermanagementsystem/Menu/Menus.java
+++ cs3343.group18.usermanagementsystem/src/usermanagementsystem/Menu/Menus.java
@@ -12,10 +12,10 @@ public class Menus {
     }
 
     //each menu for each page or function
-    
+
     public void printHomeMenu() {
-    	System.out.println();
-    	System.out.println("User Options");
+        System.out.println();
+        System.out.println("User Options");
         System.out.println("1) ");
         System.out.println("2) ");
         System.out.println("3) ");
@@ -24,10 +24,10 @@ public class Menus {
         System.out.println("0) Logout");
         System.out.println();
     }
-    
+
     public void printAdminHomeMenu() {
-    	System.out.println();
-    	System.out.println("Admin Options");
+        System.out.println();
+        System.out.println("Admin Options");
         System.out.println("11) ");//Create User
         System.out.println("12) ");//Read User Info
         System.out.println("13) ");//Update User Info
--- cs3343.group18.usermanagementsystem/src/usermanagementsystem/user_login/UserLogin.java
+++ cs3343.group18.usermanagementsystem/src/usermanagementsystem/user_login/UserLogin.java
@@ -3,53 +3,53 @@ package usermanagementsystem.user_login;
 import usermanagementsystem.dataaccess.*;
 
 public class UserLogin {
-	private boolean loggedin;	
-	private UserDao userDao;
-	//only keep username as login user will not enter username again
-	//and it is key for select info from hash table
-	private String username;
-	
-	private static UserLogin instance = new UserLogin();
-	
-	private UserLogin() {
-		this.loggedin = false;		
-		//load users based on data.json
-		this.userDao = new UserDao();
-	}
-	
-	public static UserLogin getInstance () {
-		return instance;
-	}
+    private boolean loggedin;
+    private UserDao userDao;
+    //only keep username as login user will not enter username again
+    //and it is key for select info from hash table
+    private String username;
 
-	public boolean getLoginStatus() {
-		return loggedin;
-	}
+    private static UserLogin instance = new UserLogin();
 
-	public void setLoginStatus(boolean loginStatus) {
-		this.loggedin = loginStatus;
-	}
-	
-	public UserDao getUserDao() {
-		return userDao;
-	}
+    private UserLogin() {
+        this.loggedin = false;
+        //load users based on data.json
+        this.userDao = new UserDao();
+    }
 
-	public String getUsername() {
-		return username;
-	}
+    public static UserLogin getInstance() {
+        return instance;
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	
-	public String logoutProcess(String choice) {
-		switch(choice){
-  	   	case "y":
-  	   		setLoginStatus(false);
-  	   		return "Logged out!\n";
-  	   	case "n":
-  	   		return "";
-  	   	default:
-  	   		return "Please enter \"Y\" or \"N\"!\n";
-  	   	}
-	}
+    public boolean getLoginStatus() {
+        return loggedin;
+    }
+
+    public void setLoginStatus(boolean loginStatus) {
+        this.loggedin = loginStatus;
+    }
+
+    public UserDao getUserDao() {
+        return userDao;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String logoutProcess(String choice) {
+        switch (choice) {
+            case "y":
+                setLoginStatus(false);
+                return "Logged out!\n";
+            case "n":
+                return "";
+            default:
+                return "Please enter \"Y\" or \"N\"!\n";
+        }
+    }
 }
