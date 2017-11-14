652c07c9df0dc43b7341b3affa75f97657983990
Nathan Lam_desktop <kin021360@yahoo.com.hk>
2017-11-14 23:16:16 +0800

    update admincontroller

--- cs3343.group18.usermanagementsystem/src/usermanagementsystem/controller/AdminController.java
+++ cs3343.group18.usermanagementsystem/src/usermanagementsystem/controller/AdminController.java
@@ -14,5 +14,64 @@ public class AdminController extends UserController {
         this.supervisors = supervisors;
     }
 
+    private User getUserOrSupervisor(String userName) {
+        if (users.containsKey(userName)) {
+            return users.get(userName);
+        } else if (supervisors.containsKey(userName)) {
+            return supervisors.get(userName);
+        }
+        return null;
+    }
 
+    public boolean createUserAndAdd(String userName, String password, String gender, String position, String staffId, String email, String departmentOf, String isAdmin) {
+        if (!users.containsKey(userName)) {
+
+        }
+        return false;
+    }
+
+    public boolean createSupervisorAndAdd(String userName, String password, String gender, String position, String staffId, String email, String departmentOf, String isAdmin) {
+        if (!supervisors.containsKey(userName)) {
+
+        }
+        return false;
+    }
+
+    public boolean upgradeToSupervisor(String userName) {
+        if (users.containsKey(userName) && !supervisors.containsKey(userName)) {
+            User u = users.get(userName);
+            users.remove(userName);
+//            supervisors.put(u.toSupervisor());
+        }
+        return false;
+    }
+
+    public boolean addPermissionToUser(String userName, String permission) {
+        User u = getUserOrSupervisor(userName);
+        if (u != null) {
+            try {
+                EnumPermission p = EnumPermission.valueOf(permission);
+                u.addPermission(p);
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public String getUserListResult() {
+
+        return "";
+    }
+
+    public String getSupervisorListResult() {
+
+        return "";
+    }
+
+    public String getAllResult() {
+
+        return "";
+    }
 }
