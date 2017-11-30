64ceb1d166fbde71fa63a3f6daab6b9e4552f8b1
Nam_PC <chingnam0107@gmail.com>
2017-12-01 00:28:31 +0800

    push

--- cs3343.group18.usermanagementsystem/src/usermanagementsystem/test/TestUserLogin.java
+++ cs3343.group18.usermanagementsystem/src/usermanagementsystem/test/TestUserLogin.java
@@ -7,10 +7,14 @@ import java.util.Hashtable;
 import org.junit.Test;
 
 import usermanagementsystem.user_login.*;
+import usermanagementsystem.datastructure_interface.ISupervisorInfo;
 import usermanagementsystem.datastructure_interface.IUserInfo;
 import usermanagementsystem.controller.AdminController;
 import usermanagementsystem.controller.IController;
 import usermanagementsystem.dataaccess.UserDao;
+import usermanagementsystem.datastructure.EnumDepartment;
+import usermanagementsystem.datastructure.EnumGender;
+import usermanagementsystem.datastructure.EnumPosition;
 import usermanagementsystem.datastructure.Supervisor;
 import usermanagementsystem.datastructure.User;
 import usermanagementsystem.controller.UserController;
@@ -66,63 +70,31 @@ public class TestUserLogin {
 	@Test
 	public void test_UserLogin07() throws Exception {
 		UserLogin user = UserLogin.getInstance();
-		boolean userCheck = user.checkUserExist(null);
-		assertEquals(false, userCheck);
+		IController login = user.login(null, null);
+		assertEquals(null, login);
 	}
 	
 	@Test
 	public void test_UserLogin08() throws Exception {
 		UserLogin user = UserLogin.getInstance();
-		boolean userCheck = user.checkUserExist("abc");
-		assertEquals(true, userCheck);
+		user.logoutProcess();
+		boolean status = user.getLoginStatus();
+		assertEquals(false, status);
 	}
-	
-	/*@Test
-	public void test_UserLogin08() throws Exception {
-		UserLogin user = UserLogin.getInstance();
-		String msg = user.logoutProcess("y");
-		assertEquals("Logged out!\n", msg);
-	}*/
 	
 	@Test
 	public void test_UserLogin09() throws Exception {
 		UserLogin user = UserLogin.getInstance();
-		IUserInfo info = user.getLoggedInUserInfo();
-		assertEquals(null, info);
+		String name = user.getLoggedInUsername();
+		assertEquals("", name);
 	}
 	
 	@Test
 	public void test_UserLogin10() throws Exception {
 		UserLogin user = UserLogin.getInstance();
-		String msg = user.logoutProcess("n");
-		assertEquals("", msg);
+		IController login = user.login("james", "104");
+		String name = user.getLoggedInUsername();
+		assertEquals("james", name);
 	}
-	
-	@Test
-	public void test_UserLogin11() throws Exception {
-		UserLogin user = UserLogin.getInstance();
-		String msg = user.logoutProcess("\0y");
-		assertEquals("Please enter \"Y\" or \"N\"!\n", msg);
-	}
-	
-	@Test
-	public void test_UserLogin12() throws Exception {
-		UserLogin user = UserLogin.getInstance();
-		String msg = user.logoutProcess("\0n");
-		assertEquals("Please enter \"Y\" or \"N\"!\n", msg);
-	}
-	
-	@Test
-	public void test_UserLogin13() throws Exception {
-		UserLogin user = UserLogin.getInstance();
-		String msg = user.logoutProcess("\0");
-		assertEquals("Please enter \"Y\" or \"N\"!\n", msg);
-	}
-	
-	@Test
-	public void test_UserLogin14() throws Exception {
-		UserLogin user = UserLogin.getInstance();
-		String name = user.getLoggedinUsername();
-		assertEquals("", name);
-	}
+
 }
